7190beb1fc699c0fdc24f19dacc20f50d7df346a
hvi <hvi@lesfurets.com>
2019-04-14 23:06:32 +0200

    Updates

---
+++ precache-manifest.b57a638211f843e76988c35b202fb371.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "45886927da64cbdb31e9",
+    "url": "/qr/static/css/main.af4baad9.chunk.css"
+  },
+  {
+    "revision": "45886927da64cbdb31e9",
+    "url": "/qr/static/js/main.98004851.chunk.js"
+  },
+  {
+    "revision": "14143d3ddc445f1fe6f2",
+    "url": "/qr/static/js/runtime~main.f53ae5be.js"
+  },
+  {
+    "revision": "3103d19afb471013fb55",
+    "url": "/qr/static/js/2.2747edcf.chunk.js"
+  },
+  {
+    "revision": "bc973f1bc0de79594bd0761cdb2e4ed4",
+    "url": "/qr/static/media/qr-code.bc973f1b.svg"
+  },
+  {
+    "revision": "210fc0cc46cbc37b99492340cf3b8cab",
+    "url": "/qr/static/media/list.210fc0cc.svg"
+  },
+  {
+    "revision": "532367b0b260369d669b14ae185d16ac",
+    "url": "/qr/static/media/create.532367b0.svg"
+  },
+  {
+    "revision": "70168e4191234fcb228e3fe8f6e05463",
+    "url": "/qr/index.html"
+  }
+];
